f6272650b9a407445b953effd70674269c1a4c71
vibha2 <juhisahu2203@gmail.com>
2023-05-28 19:57:01 +0530

    unllike Posts done

--- controller/likeController.js
+++ controller/likeController.js
@@ -23,7 +23,33 @@ exports.likePost = async (req,res) => {
     }
     catch(error){
         return res.status(500).json({
-            error: "Error while Creating comment"
+            error: "Error while liking post"
+        })
+
+    }
+}
+
+//Unlike a post
+exports.unlikePost = async(req,res) => {
+    try{
+        const {post, like} = req.body;
+        //find and delete the like collection me se
+        const deletedLike = await Like.findOneAndDelete(
+            {post:post, _id:like}
+        );
+
+        //update the pot collection
+        const updatedPost = await Post.findByIdAndUpdate(post,
+            {$pull: {likes: deletedLike._id}}, {new:true});
+
+        res.json({
+            post: updatedPost,
+        })
+    }
+    catch(error)
+    {
+        return res.status(500).json({
+            error: "Error while unlike post"
         })
 
     }
@@ -40,4 +66,12 @@ exports.likePost = async (req,res) => {
 // {
 //     "post": "6473401198f311ce6c1ac612",
 //     "user":  "Praveen Sahu"
+// }
+
+//Testing for unlike
+//  http://localhost:4000/api/v1/likes/unlike
+// post type
+// {
+//     "post": "6473401198f311ce6c1ac612",
+//     "like":  "647356300c675a06cfbbedfe"
 // }
--- routes/blog.js
+++ routes/blog.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const { createComment } = require("../controller/CommentController");
 // const { dummyController } = require("../controller/LikeController");
 const { createPost, getAllPosts } = require("../controller/PostController");
-const { likePost } = require("../controller/LikeController");
+const { likePost, unlikePost } = require("../controller/LikeController");
 
 // router.get("/dummyRoutes", dummyController);
 router.post("/comments/create", createComment );
 router.post("/posts/create", createPost);
 router.get("/posts", getAllPosts);
 router.post("/likes/like", likePost);
+router.post("/likes/unlike", unlikePost);
 // router.post("/posts", postController);
 
 module.exports = router;
