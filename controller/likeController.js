da77c08987c8a95ea5c3c50187a35e4c76adb472
vibha2 <juhisahu2203@gmail.com>
2023-05-28 19:05:16 +0530

    like controller created

--- controller/likeController.js
+++ controller/likeController.js
@@ -1,6 +1,43 @@
+//import models
+const Post = require("../model/postModel");
+const Like = require("../model/likeModel");
 
+//like a post
+exports.likePost = async (req,res) => {
+    try{
+        const {post, user} = req.body;
+        const like = new Like({
+            post, user,
+        });
+
+        const savedLike = await like.save();
+
+        //update the post collection basis on this
+        const updatedPost = await Post.findByIdAndUpdate(post,
+         {$push: {likes: savedLike._id} },
+         {new: true}).populate("likes").exec();
+
+         res.json({
+            post: updatedPost,
+         })
+    }
+    catch(error){
+        return res.status(500).json({
+            error: "Error while Creating comment"
+        })
+
+    }
+}
 
 // exports.dummyController = async(req,res) => {
 
 //     res.send("This is your Dummy Page");
+// }
+
+//Testing for Like
+//  http://localhost:4000/api/v1/likes/like
+// post type
+// {
+//     "post": "6473401198f311ce6c1ac612",
+//     "user":  "Praveen Sahu"
 // }
--- routes/blog.js
+++ routes/blog.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const { createComment } = require("../controller/CommentController");
-const { dummyController } = require("../controller/LikeController");
+// const { dummyController } = require("../controller/LikeController");
 const { createPost, getAllPosts } = require("../controller/PostController");
+const { likePost } = require("../controller/LikeController");
 
-router.get("/dummyRoutes", dummyController);
+// router.get("/dummyRoutes", dummyController);
 router.post("/comments/create", createComment );
 router.post("/posts/create", createPost);
 router.get("/posts", getAllPosts);
+router.post("/likes/like", likePost);
 // router.post("/posts", postController);
 
 module.exports = router;
